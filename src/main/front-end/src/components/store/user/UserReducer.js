974e4ee7bac0162e217c81ea7105f42767241538
Kostas <kpapadim27@gmail.com>
2023-12-16 17:55:30 +0200

    redux completed remains the connections

---
+++ src/main/front-end/src/components/store/user/UserReducer.js
@@ -0,0 +1,33 @@
+import {
+  FETCH_USER_FAILURE,
+  FETCH_USER_REQUEST,
+  FETCH_USER_SUCCESS,
+} from "./UserType";
+
+const initialState = {
+  user: [],
+  error: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+      return {
+        ...state,
+      };
+    case FETCH_USER_SUCCESS:
+      return {
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USER_FAILURE:
+      return {
+        users: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
